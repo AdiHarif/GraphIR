c1b211ee50858f94eb9cf42727799f14e262f2f2
Adi Harif <adi.harif@campus.technion.ac.il>
2023-10-03 11:24:54 +0300

    Added support for subgraphes in relations output

--- src/output/relations.ts
+++ src/output/relations.ts
@@ -2,11 +2,7 @@
 import * as csv_writer from 'csv-writer'
 import { Graph } from '../graph.js'
 
-export async function exportIrToRelations(graph: Graph, dir: string) {
-    const verticesPath = `${dir}/vertices.facts`
-    const verticesWriter = csv_writer.createArrayCsvWriter({path: verticesPath})
-    const edgesPath = `${dir}/edges.facts`
-    const edgesWriter = csv_writer.createArrayCsvWriter({path: edgesPath})
+export async function writeRecordsToFiles(graph: Graph, verticesWriter: any, edgesWriter: any) {
 
     const vertices: Array<Array<any>> = [];
     const edges: Array<Array<any>> = [];
@@ -15,11 +11,22 @@ export async function exportIrToRelations(graph: Graph, dir: string) {
         vertices.push([ v.id, v.kind, v.category, v.label ]);
         edges.push(...v.outEdges.map(e=> [ e.source.id, e.target!.id, e.category, e.label ]));
     })
+
     await Promise.all([
         await verticesWriter.writeRecords(vertices),
         await edgesWriter.writeRecords(edges)
     ]);
 
+    for (const subgraph of graph.subgraphs)  {
+        await writeRecordsToFiles(subgraph, verticesWriter, edgesWriter);
+    }
+}
 
 
+export async function exportIrToRelations(graph: Graph, dir: string) {
+    const verticesPath = `${dir}/vertices.facts`
+    const verticesWriter = csv_writer.createArrayCsvWriter({path: verticesPath})
+    const edgesPath = `${dir}/edges.facts`
+    const edgesWriter = csv_writer.createArrayCsvWriter({path: edgesPath})
+    await writeRecordsToFiles(graph, verticesWriter, edgesWriter);
 }
