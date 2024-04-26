ebf6411d746365826dee05ec178d2e66a9a531d1
Adi Harif <adi.harif@campus.technion.ac.il>
2024-04-27 02:40:23 +0300

    Added array types to module exports

--- src/module_exports.ts
+++ src/module_exports.ts
@@ -6,6 +6,7 @@ export * from './vertex/pass_vertex.js';
 export * from './vertex/vertex_visitor.js';
 export * from './type/type.js';
 export * from './type/type_visitor.js';
+export * from './type/array_type.js';
 export * from './edge.js';
 export * from './output/relations.js';
 export * from './output/dot.js';
